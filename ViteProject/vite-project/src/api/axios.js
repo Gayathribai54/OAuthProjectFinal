ab4da83fe341992566e6d03a3ea71f904ac75abf
Gayathribai54 <gayathrithakur95@gmail.com>
2026-01-29 09:25:56 -0800

    Updated code

--- ViteProject/vite-project/src/api/axios.js
+++ ViteProject/vite-project/src/api/axios.js
@@ -1,8 +1,9 @@
 
 import axios from "axios";
-
+import dotenv from "dotenv"
+dotenv.config();
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
