e15f468b579eb69819e6466cd3c25779d1a6810d
Gayathribai54 <gayathrithakur95@gmail.com>
2026-02-07 04:36:16 -0800

    update

--- ViteProject/vite-project/src/pages/Login.jsx
+++ ViteProject/vite-project/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 
 const Login = () => {
   const loginWithGoogle = () => {
-    window.location.href = "http://localhost:5000/auth/google";
+    window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
   };
 
   return (
